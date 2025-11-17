ef1d7c94ba6b93961096f29942385c2f9d0f6d39
caiofabrasil <>
2025-11-17 13:36:14 -0300

    Color Switch

---
+++ Assets/VFXManager.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VFXManager : MonoBehaviour
+{
+    
+    [SerializeField]
+    private List<GameObject> VFXs = new List<GameObject>();
+    
+    [SerializeField]
+    private List<ColorProfile> ColorProfiles = new List<ColorProfile>();
+
+    [SerializeField] private List<Light> ParticleLight = new List<Light>();
+    
+    public Slider slider;
+    
+    public void ChangeColor()
+    {
+        foreach (Light light in ParticleLight)
+        {
+            light.color = ColorProfiles[(int)slider.value].LightColor;
+        }
+        
+            foreach (var VFX in VFXs)
+            {
+                foreach (var rend in VFX.GetComponentsInChildren<Renderer>(true))
+                {
+                    Material mat = rend.material;
+                    string shaderName = mat.shader.name;
+
+                    foreach (ColorProfile.ColorEntry entry in ColorProfiles[(int)slider.value].VFXColors)
+                    {
+                        if (shaderName == entry.ShaderName) 
+                        {
+                            mat.SetColor(entry.ShaderVariable, entry.color);
+                        }
+                    }
+                }
+            }
+        
+        
+    }
+}
